4062e37cc56dbbecbec03e58c3e0021bb320cfc5
Derek Musselmann <158748+phrac@users.noreply.github.com>
2020-01-20 17:56:31 -0600

    add __str__ to work order model

--- workorders/models.py
+++ workorders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from maintdx.assets.models import Asset
 
-# Create your models here.
+class WorkOrderType(models.Model):
+    name = models.CharField(max_length=32)
+    short_name = models.CharField(max_length=4)
+   
+    def __str__(self):
+        return self.name
+
+class WorkOrder(models.Model):
+    asset = models.ForeignKey(Asset, on_delete=models.CASCADE)
+    wo_type = models.ForeignKey(WorkOrderType, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
