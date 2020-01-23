90f48647484ec29db0faa09f2c26d71b0c769545
Derek Musselmann <158748+phrac@users.noreply.github.com>
2020-01-23 13:50:32 -0600

    Add basic time tracking to work orders

--- workorders/models.py
+++ workorders/models.py
@@ -14,10 +14,22 @@ class WorkOrder(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     closed = models.BooleanField(default=False)
+    closed_at = models.DateTimeField(null=True, blank=True)
     due_date = models.DateField(null=True)
     description = models.CharField(max_length=256, blank=True, null=True)
-    work_instructions = models.TextField(null=True)
+    work_instructions = models.TextField(null=True, blank=True)
+    tech_notes = models.TextField(null=True, blank=True)
    
 
     def __str__(self):
         return self.name
+
+class WorkOrderClock(models.Model):
+    work_order = models.ForeignKey(WorkOrder, on_delete=models.CASCADE)
+    clock_in = models.DateTimeField(auto_now_add=True)
+    clock_out = models.DateTimeField(null=True, blank=True)
+    interval = models.DurationField(null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        self.interval = self.clock_out - self.clock_in
+        super(WorkOrderClock, self).save(*arg, **kwargs)
