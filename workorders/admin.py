9a79fcfdad670232f4bdff81aff3f4e76aa2785a
Derek Musselmann <158748+phrac@users.noreply.github.com>
2020-02-04 07:28:12 -0600

    register workorders in admin

--- workorders/admin.py
+++ workorders/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+import maintdx.workorders.models as wo
 
-# Register your models here.
+admin.site.register(wo.WorkOrder)
+admin.site.register(wo.WorkOrderType)
+admin.site.register(wo.WorkOrderPart)
+admin.site.register(wo.WorkOrderClock)
